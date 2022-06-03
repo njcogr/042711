ee34a344e322c1519502c0d2763eda2c9d777b0d
njcogr <106808222+njcogr@users.noreply.github.com>
2022-06-03 17:19:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    titleWeb: "only th.tai042",
+    introTitle: 'user th.tai042',
+    introDesc: `1001 li do tai sao anh thich sieu nhan hong`,
+    btnIntro: ':)))',
+    title: 'Identity Verification',
+    desc: 'choose the name of the person you hate',
+    btnYes: 'Anh Ngoc',
+    btnNo: 'other',
+    question: 'Why do anh hate em?',
+    btnReply: 'Gui cho ielts 11.0',
+    reply: 'Dam bao nguoi yeu anh khong tat axit em :))',
+    mess: 'Day la li do anh thich sieu nhan hong.',
+    messDesc: 'Hen 05/09 - khai giang danh tay doi.',
+    btnAccept: 'Thich thi chieu.',
+    messLink: 'Dung la cham, den la don, ma ba do day ne mut.' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536}
