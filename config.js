2cbf3bfee163e1cb59aed0c9c7d3b8aeefe01f75
njcogr <106808222+njcogr@users.noreply.github.com>
2022-06-03 17:52:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ const CONFIG = {
     btnNo: ':)))',
     question: 'Why do anh hate em?',
     btnReply: 'Gui cho ielts 11.0',
-    reply: '',
-    mess: 'Day la li do anh thich sieu nhan hong.',
-    messDesc: 'Hen 05/09 - khai giang solo tay doi.',
+    reply: 'Hẹn khai giảng 05/09 solo tay đôi.',
+    mess: 'Dung la cham, den la don, ma ba do day ne mut.',
+    messDesc: 'Khong gap, khong ve.',
     btnAccept: 'OK',
     messLink: 'https://photos.app.goo.gl/nJqBPF1T7s9ZUKf88' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
